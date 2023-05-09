c8375f7aaa572673e1e9c692ebd6b26ef221b64e
Aaron Criner <aaron.criner@me.com>
2023-05-08 17:17:03 -0700

    added information on validations, created custom validation to check for username uniqueness

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,12 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user';
 import { Profile } from 'src/profiles/entities/profile.entity';
 import { Post } from 'src/posts/entities/post';
+import { UsernameUniqueness } from 'src/users/validators/isUserUnique';
 
 
 @Module({
   //
   imports: [TypeOrmModule.forFeature([User, Post, Profile])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsernameUniqueness],
 })
 export class UsersModule {}
