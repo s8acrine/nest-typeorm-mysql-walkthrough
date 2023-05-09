b9d3124bb3ab6395a4ec5e9b017aec1622319b6d
Aaron Criner <aaron.criner@me.com>
2023-05-09 09:24:39 -0700

    fixed misnamed import and function name

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,10 +1,10 @@
 import { IsString, Length } from "class-validator";
-import { IsUserUnique } from "src/users/validators/isUserUnique";
+import { isUserUnique } from "src/users/validators/isUserUnique";
 
 export class CreateUserDto {
   @IsString()
   @Length(3, 20)
-  @IsUserUnique()
+  @isUserUnique()
   username: string;
 
   @IsString()
