e9c78546f189429ccf6be24e75d28cb6cfb14cc3
Aaron Criner <aaron.criner@me.com>
2023-05-10 10:35:02 -0700

    added transform:true to pipes implementation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { useContainer } from 'class-validator';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
   await app.listen(3000);
 }
 bootstrap();
