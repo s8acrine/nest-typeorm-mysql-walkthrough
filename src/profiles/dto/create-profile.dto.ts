bb2a701f7d31c514828ca4ed7bd86a30b638b0d3
Aaron Criner <aaron.criner@me.com>
2023-05-10 10:36:28 -0700

    added type transformation to date field

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,5 +1,12 @@
+import { Type } from "class-transformer";
+import { IsDate, IsString, isDateString } from "class-validator";
+
 export class CreateProfileDto {
+  @IsString()
   firstName: string;
+  @IsString()
   lastName: string;
+  @Type(() => Date)
+  @IsDate()
   dateOfBirth: Date;
 }
